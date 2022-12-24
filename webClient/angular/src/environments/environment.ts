8396946fede1a74a8c2ff3ffa31390df3a50b547
Zameel Hassan <43750093+zameel7@users.noreply.github.com>
2022-12-24 09:53:37 +0530

    Update environment.ts

--- webClient/angular/src/environments/environment.ts
+++ webClient/angular/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   region: 'ap-northeast-1',
-  userPoolId: 'ap-northeast-1_d8Bp7NjSF',
-  userPoolWebClientId: '637mi15a121tpukqn4kd94mtcf',
+  userPoolId: 'ap-northeast-1_3sfuHZ29y',
+  userPoolWebClientId: 'abcue1u6fun9a7dhutrgnus39',
 };
 
 /*
